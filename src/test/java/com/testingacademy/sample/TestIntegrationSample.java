597f6bb7c6a93ec7c001a647b710d3585a81879b
Janvi Gogiya <jahnvigogia@gmail.com>
2025-06-29 19:16:57 +0530

    TestNG

---
+++ src/test/java/com/testingacademy/sample/TestIntegrationSample.java
@@ -0,0 +1,46 @@
+package com.testingacademy.sample;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestIntegrationSample {
+
+    //Create a Booking, Create a token
+    //Verify that get booking
+    //Update the Booking
+    //Delete the Booking
+
+    @Test (groups = "QA", priority = 1)
+    @Owner("Jennie")
+    @Description ("TC#INT1 = Step 1. Verify that Booking can be created")
+
+    public void testCreateBooking(){
+        Assert.assertTrue(true);
+    }
+
+    @Test (groups = "QA", priority = 2)
+    @Owner("Jennie")
+    @Description ("TC#INT1 = Step 2. Verify that Booking by ID")
+
+    public void testVerifyBooking(){
+        Assert.assertTrue(true);
+    }
+
+    @Test (groups = "QA", priority = 3)
+    @Owner("Jennie")
+    @Description ("TC#INT1 = Step 3. Verify Update Booking by ID")
+
+    public void testUpdateBooking(){
+        Assert.assertTrue(true);
+    }
+
+    @Test (groups = "QA", priority = 4)
+    @Owner("Jennie")
+    @Description ("TC#INT1 = Step 4. Delete that booking by ID")
+
+    public void testDeleteBooking(){
+        Assert.assertTrue(true);
+    }
+}
